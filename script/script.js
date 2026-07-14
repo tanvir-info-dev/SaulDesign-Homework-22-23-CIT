c6d85ba6acaa0bde62749a8f489577bfb44bbf85
tanvir-info-dev <tanvir.info.dev@gmail.com>
2026-07-14 21:12:50 +0600

    my recent works section completed and menu links added

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("readBtn");
 const moreText = document.getElementById("more");
 const dots = document.getElementById("dots");
+const buttons = document.querySelectorAll(".filter-btn");
+const cards = document.querySelectorAll(".w_card");
 
 btn.addEventListener("click", function () {
     if (moreText.style.display === "inline") {
@@ -12,4 +14,35 @@ btn.addEventListener("click", function () {
         dots.style.display = "none";
         btn.textContent = "Read Less";
     }
+});
+
+// Works Showcase
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        // Active button
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        // Filter value
+        const filter = button.dataset.filter;
+
+        cards.forEach(card => {
+
+            if(filter === "all"){
+                card.classList.remove("hide");
+            }
+            else if(card.classList.contains(filter)){
+                card.classList.remove("hide");
+            }
+            else{
+                card.classList.add("hide");
+            }
+
+        });
+
+    });
+
 });
